b1cc647df24dcf2deab26bcb9a4718143e469ca1
Jaison Justus <jaison.justus.lp@gmail.com>
2017-10-03 15:51:12 +0530

    Adding utility method to extract hostname

--- js/todotab.jsx
+++ js/todotab.jsx
@@ -3,6 +3,36 @@
 var TodoStore = new Store('TodoTab-Tasks');
 var ActivityStore = new Store('TodoTab-Activities');
 
+function extractHostname(url) {
+	var hostname;
+
+	if (url.indexOf("://") > -1) {
+			hostname = url.split('/')[2];
+	}
+	else {
+			hostname = url.split('/')[0];
+	}
+
+	hostname = hostname.split(':')[0];
+	hostname = hostname.split('?')[0];
+
+	return hostname;
+}
+
+function extractRootDomain(url) {
+	var domain = extractHostname(url),
+			splitArr = domain.split('.'),
+			arrLen = splitArr.length;
+
+	if (arrLen > 2) {
+			domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+			if (splitArr[arrLen - 1].length == 2 && splitArr[arrLen - 1].length == 2) {
+					domain = splitArr[arrLen - 3] + '.' + domain;
+			}
+	}
+	return domain;
+}
+
 var Layout = React.createClass({
 
 	layouts: [
