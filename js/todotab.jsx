61a7743893a5dd76693596e4f187bdeef6fcf6f8
Jaison Justus <jaison.justus.lp@gmail.com>
2017-10-05 00:46:57 +0530

    Adding handle style and icon components

--- js/todotab.jsx
+++ js/todotab.jsx
@@ -497,7 +497,7 @@ Todolist.ToggleList = React.createClass({
 	render: function()	{
 		return (
 			<li className="Todo Todo--toggle" onClick={this.props.onClick}>
-				{(this.props.toggleMode) ? 'show more' : 'show less'}
+				{(this.props.toggleMode) ? 'View All' : 'Roll Up'}
 			</li>
 		);
 	}
@@ -566,6 +566,9 @@ Todolist.Item = React.createClass({
 
 			return (
 					<li className={todoClasses}>
+							<ol className="Todo__Handle">
+								<li></li> <li></li> <li></li>
+							</ol>
 							<span className="Todo__Check">
 									<i onClick={this.handleCheck.bind(this)}></i>
 							</span>
@@ -604,16 +607,33 @@ var Footer = React.createClass({
 		this.props.goto(link);
 	},
 
+	icon: function(link)	{
+		console.log(link)
+		switch(link)	{
+			case 'customize':
+				return <Icon.Settings/>
+			case 'help':
+				return <Icon.Help/>
+			case 'home':
+			return <Icon.List/>
+		}
+	},
+
 	links: function()	{
+		var linksItemClasses = {};
+
 		return this.props.layouts.map(function(layout)	{
-			if(layout.link !== this.props.layout)	{
-				return (
-					<li className="Links__Item"
-						onClick={this.goto.bind(this, layout.link)}>
-						{layout.name}
-					</li>
-				);
-			}
+			linksItemClasses = classNames({
+				"Links__Item": true,
+				"Links__Item--selected": (layout.link === this.props.layout)
+			});
+			
+			return (
+				<li className={linksItemClasses}
+					onClick={this.goto.bind(this, layout.link)}>
+					{this.icon(layout.link)}
+				</li>
+			);
 		}, this);
 	},
 
@@ -760,6 +780,41 @@ Pallete.Swatch = React.createClass({
 		return (
 			<li className={swatchClasses} onClick={this.handleClick.bind(this)}></li>
 		)
+	}
+});
+
+var Icon = {};
+
+Icon.Settings = React.createClass({
+	render: function()	{
+		return (
+			<svg className="Icon Icon--settings" fill="#000000" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
+				<path d="M0 0h24v24H0z" fill="none"/>
+				<path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z"/>
+			</svg>
+		);
+	}
+});
+
+Icon.Help = React.createClass({
+	render: function()	{
+		return (
+			<svg className="Icon Icon--help" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+				<path d="M0 0h24v24H0z" fill="none"/>
+				<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/>
+			</svg>
+		);
+	}
+});
+
+Icon.List = React.createClass({
+	render: function()	{
+		return (
+			<svg className="Icon Icon--list" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+				<path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"/>
+				<path d="M0 0h24v24H0z" fill="none"/>
+			</svg>
+		);
 	}
 });
 
