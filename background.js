d33851499438a25ffbf1269b6e6f2e167a13e67e
Suyash Katiyar <katiyar.suyash@gmail.com>
2017-11-10 15:54:34 +0530

    changed back to chrome

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-browser.runtime.onInstalled.addListener(function(res) {
+chrome.runtime.onInstalled.addListener(function(res) {
     chrome.tabs.create({
         url: chrome.extension.getURL('welcome.html')
     })
 });
 
-browser.runtime.setUninstallURL("http://jaison.info/projects/todotab/uninstalled.html");
+chrome.runtime.setUninstallURL("http://jaison.info/projects/todotab/uninstalled.html");
